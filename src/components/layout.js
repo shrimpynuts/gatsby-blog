8292c3f0f8efe0818ad329f19e908efce9d16605
Jonathan Cai <caimjonathan@gmail.com>
2020-10-13 22:51:48 -0700

    initial port

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,30 +4,19 @@ import { Link } from "gatsby"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  
+  let header = (<h6 className="main-heading">
+    <Link to="/">Jonathan Cai</Link>
+  </h6>);
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        {/* © {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://www.gatsbyjs.com">Gatsby</a> */}
       </footer>
     </div>
   )
