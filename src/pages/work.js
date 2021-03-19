05c708355e2ca04297af25a1ac04c299d1ed70b5
Jonathan Cai <caimjonathan@gmail.com>
2021-03-19 00:52:51 -0700

    update work

--- src/pages/work.js
+++ src/pages/work.js
@@ -11,46 +11,24 @@ const Work = ({ data, location }) => {
             <SEO title="Work" />
             <h1>Work</h1>
             <div className="section-content">
-                <ul>
-                    <li>
-                        <p>
-                            At <b>Facebook</b>, I worked on their <b>Network Infra team</b>, improving the performance and reliability of a service that generates network device configs. <br />
-                The service handled ~3k requests/hour. My work saw a ~10% performance increase, and prevented several SEVs.
-            </p>
-                    </li>
+                <p>At <b>Facebook</b>, I worked on their <b>Network Infra team</b>, automating the deployment of network configs. </p>
 
-                    <li>
-                        <p>
-                            At <b>JPMorgan</b>, I worked on <b>data modeling</b>, and <b>data engineering at scale</b>. <br />
-                This looked like 12,000+ data points with 40+ properties on an hourly basis. <br />
-                Dealt with constantly changing business requirements from investment bankers, varying data processing issues (asynchronous fetching on dynamic time cycles), etc.
-            </p>
-                    </li>
+                <p>At <b>JPMorgan</b>, I worked on <b>data modeling</b>, and <b>data engineering</b> at scale.</p>
 
-                    <li>
-                        <p>
-                            At <b><a href="https://www.sihuatech.com/" rel="noopener noreferrer" target="_blank">Sihua Technologies</a></b>,
-                I improved the deployment time for their <b>CI/CD</b> pipeline by 70%, but the tool was completely unmanagable, the code was absolute crap, and not used in production.
-            </p>
-                    </li>
-                </ul>
+                <p>
+                    At <b><a href="https://www.sihuatech.com/" rel="noopener noreferrer" target="_blank">Sihua Technologies</a></b>,
+                    I improved the deployment time for their <b>CI/CD</b> pipeline</p>
 
-                <ul>
-                    <li>
-                        <p>
-                            I also run an app development club at Rice called <b><a target="_blank" rel="noopener noreferrer" href="http://riceapps.org/">RiceApps</a></b>.
-                We're about 50 members strong now, and we're building some pretty ambitious projects this year.
-            </p>
-                    </li>
-                    <li>
+                <p>
+                    I also run an app development club at Rice called <b><a target="_blank" rel="noopener noreferrer" href="http://riceapps.org/">RiceApps</a></b>.
+                    We're about 50 members strong now, and we're building some pretty {" "}
+                    <b><a target="_blank" rel="noopener noreferrer"  href="https://medium.com/riceapps/riceapps-2020-2021-projects-a9b705391790">ambitious projects</a></b> this year.
+                </p>
 
-                        <p>
-                            At Rice, I've served as a <b>TA</b> for a few CS courses (head TA for COMP 322 <b>Parallel Programming</b>,
-                        and COMP 330 <b>Data Science</b>) and had an unfortunately short and hardly significant <b>undergrad research</b> stint in <b>deep learning</b>.
-            </p>
-                    </li>
-                </ul>
-
+                <p>
+                    While studying at Rice, I <b>TA'd</b> for a few CS courses (head TA for <b>Parallel Programming</b>,
+                    and <b>Data Science</b>) and had a short <b>undergrad research</b> stint in <b>deep learning</b>.
+                </p>
             </div>
         </Layout>
     )
