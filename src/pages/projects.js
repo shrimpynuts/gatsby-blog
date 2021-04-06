3cfae424edcc34ffac3ef122c1760501110ea6ae
Jonathan Cai <caimjonathan@gmail.com>
2021-04-06 13:37:46 -0700

    projects bs

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,38 +13,44 @@ const Projects = ({ data: _, location }) => {
 
       <div>
         <table className="project-table">
-          <tr>
-            <th> </th>
-            <th>Name</th>
-            <th className="hidden-column-mobile">Date</th>
-            <th className="hidden-column-mobile">Description</th>
-            <th className="hidden-column-mobile">Status</th>
-            <th>Link</th>
-            {/* <th>Results</th> */}
-          </tr>
-          {projectData.map((project, i) => {
-            return (
-              <tr>
-                <td>
-                  <img
-                    src={project.icon || "https://i.imgur.com/s7ajAQw.png"}
-                    style={{ width: "30px", marginRight: "10px" }}
-                    alt=""
-                  />
-                </td>
-                <td>{project.name}</td>
-                <td className="hidden-column-mobile">{`${project.month} ${project.year}`}</td>
-                <td className="hidden-column-mobile">{project.description}</td>
-                <td className="hidden-column-mobile">{project.status}</td>
-                <td>
-                  <a href={project.link} target="_blank" rel="noreferrer">
-                    {project.link_display}
-                  </a>
-                </td>
-                {/* <td>{project.results}</td> */}
-              </tr>
-            )
-          })}
+          <thead>
+            <tr>
+              <th> </th>
+              <th>Name</th>
+              <th className="hidden-column-mobile">Date</th>
+              <th className="hidden-column-mobile">Description</th>
+              <th className="hidden-column-mobile">Status</th>
+              <th>Link</th>
+              {/* <th>Results</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {projectData.map((project, i) => {
+              return (
+                <tr key={i}>
+                  <td>
+                    <img
+                      src={project.icon || "https://i.imgur.com/s7ajAQw.png"}
+                      style={{ width: "30px", marginRight: "10px" }}
+                      alt=""
+                    />
+                  </td>
+                  <td>{project.name}</td>
+                  <td className="hidden-column-mobile">{`${project.month} ${project.year}`}</td>
+                  <td className="hidden-column-mobile">
+                    {project.description}
+                  </td>
+                  <td className="hidden-column-mobile">{project.status}</td>
+                  <td>
+                    <a href={project.link} target="_blank" rel="noreferrer">
+                      {project.link_display}
+                    </a>
+                  </td>
+                  {/* <td>{project.results}</td> */}
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
       </div>
     </Layout>
